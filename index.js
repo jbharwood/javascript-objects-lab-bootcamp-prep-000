a5051eb2094cfd18eab91640606d42636dc68ef5
Joseph Harwood <josephblakeharwood@gmail.com>
2018-08-16 20:29:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 function deleteFromObjectByKey(object, key) {
-  var obj = { [key] ' '}
+  var obj = { [key]: ''}
   return Object.assign({}, object, obj)
 }
