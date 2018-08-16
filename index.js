38cfa92f593e3af688ff7e45a240e45ff729f7fc
Joseph Harwood <josephblakeharwood@gmail.com>
2018-08-16 19:59:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var recipes = { key: "object" }
 function updateObjectWithKeyAndValue(object, key, value) {
   var obj = { key: value}
   object = Object.assign({}, obj)
-  return
+  return object
 }
